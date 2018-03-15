119f68077620e72209a6ac2bdfed48effec95561
Kelsey Fecho <kfecho30@gmail.com>
2018-03-15 16:24:56 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,6 +4,8 @@ class Patient
       @name = name
       @appointments = []
     end
-    def add_appointment
+    def add_appointment(appointment)
+      @appointments << appointment
+      appointment.patient = self
     end
 end
